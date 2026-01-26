fab0a0643a574fdfd7f03b64e66814c73b66a5f5
Radu Macocian (admac) <admac@odoo.com>
2026-01-26 15:24:51 +0100

    fixed code companion

--- .config/nvim/after/plugin/codecompanion.lua
+++ .config/nvim/after/plugin/codecompanion.lua
@@ -1,23 +1,26 @@
 require('codecompanion').setup({
+    adapters = {
+        http = {
+            webui = function()
+                return require("codecompanion.adapters").extend("openai_compatible", {
+                    env = {
+                        url = "https://openwebui.estatecloud.org",
+                        api_key = "sk-c7e3b3c942b34528a486eb443d200de6",
+                        chat_url = "/api/chat/completions",
+                    },
+                    schema = {
+                        model = {
+                            default = "deepseek-coder:6.7b", -- Use the exact model name from your PC
+                        }
+                    }
+                })
+            end,
+        }
+    },
     interactions = {
         chat = {
-            adapter = "ollama",
+            adapter = "webui",
             model = "deepseek-coder:6.7b"
         },
-    },
-    adapters = {
-        ollama = function()
-            return require("codecompanion.adapters").extend("openai", {
-                env = {
-                    url = "https:openwebui.estatecloud.org",
-                    api_key = "sk-c7e3b3c942b34528a486eb443d200de6"
-                },
-                schema = {
-                    model = {
-                        default = "deepseek-coder:6.7b", -- Use the exact model name from your PC
-                    }
-                }
-            })
-        end,
     }
 })
