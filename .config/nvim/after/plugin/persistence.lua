8d158310a2c5f891c844f911eb82f8d5df75bc30
Radu Macocian (admac) <admac@odoo.com>
2026-01-28 16:31:13 +0100

    made .git project root for persistence

--- .config/nvim/after/plugin/persistence.lua
+++ .config/nvim/after/plugin/persistence.lua
@@ -1,5 +1,14 @@
+local function get_root ()
+    local root = vim.fs.root(0, ".git")
+    return root or vim.fn.getcwd()
+end
+
 vim.keymap.set("n", "<leader>ql", function() require("persistence").load() end)
-vim.keymap.set("n", "<leader>qs", function() require("persistence").select() end)
+vim.keymap.set("n", "<leader>qs", function()
+    vim.api.nvim_set_current_dir(get_root())
+    require("persistence").save()
+    require("persistence").select()
+end)
 vim.keymap.set("n", "<leader>qL", function() require("persistence").load({ last = true }) end)
 vim.keymap.set("n", "<leader>sq", function() require("persistence").stop() end)
 
