618a85305d2af82e040c9de1221297610969d7ca
Radu Macocian (admac) <admac@odoo.com>
2026-03-03 10:45:27 +0100

    removed local api key

--- .config/nvim/after/plugin/codecompanion.lua
+++ .config/nvim/after/plugin/codecompanion.lua
@@ -5,7 +5,6 @@ require('codecompanion').setup({
                 return require("codecompanion.adapters").extend("openai_compatible", {
                     env = {
                         url = "https://openwebui.estatecloud.org",
-                        api_key = "sk-c7e3b3c942b34528a486eb443d200de6",
                         chat_url = "/api/chat/completions",
                     },
                     schema = {
@@ -30,37 +29,6 @@ require('codecompanion').setup({
         chat = {
             adapter = "codex",
             model = "gpt-5",
-
-            keymaps = {
-                yolo_mode = false,
-            },
-
-            tools = {
-                ["insert_edit_into_file"] = {
-                    opts = {
-                        require_approval_before = {
-                            buffer = true, -- ask before editing current buffer
-                            file = true, -- ask before editing files
-                        },
-                        require_confirmation_after = true,
-                        allowed_in_yolo_mode = false,
-                    },
-                },
-                ["create_file"] = {
-                    opts = {
-                        require_approval_before = true,
-                        require_cmd_approval = true,
-                        allowed_in_yolo_mode = false,
-                    },
-                },
-                ["delete_file"] = {
-                    opts = {
-                        require_approval_before = true,
-                        require_cmd_approval = true,
-                        allowed_in_yolo_mode = false,
-                    },
-                },
-            },
         },
     }
 })
